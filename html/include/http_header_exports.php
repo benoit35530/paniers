f55056a645ebd1fa206226c1105d469adfbbd55b
Benoit Foucher <benoit@zeroc.com>
2023-02-26 10:59:07 +0100

    Minor fix

--- html/include/http_header_exports.php
+++ html/include/http_header_exports.php
@@ -8,18 +8,7 @@ if (isset($export) && $export == "excel")
     header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
     header("Pragma: public");
 }
-elseif (isset($export) && $export == "pdf")
-{
-    // ob_clean();
-    // flush();
-    // header("Content-Type: application/pdf");
-    // header("Content-Disposition: inline; filename=export_" . $action . "_" . gmdate("md") . ".pdf");
-    // header("Content-Transfer-Encoding: binary");
-    // header("Accept-Ranges: bytes");
-    // header('Expires: 0');
-    // header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-}
-else
+elseif (!isset($export) || $export != "pdf")
 {
     header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
     header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
